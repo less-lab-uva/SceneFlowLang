24a8d30ccce94cbe6fe37bd1126b5e9f37e94d08
Trey Woodlief <adwiii7@gmail.com>
2024-09-20 15:56:29 -0400

    update

--- run.sh
+++ run.sh
@@ -4,3 +4,8 @@ conda activate scene_flow
 
 ### Pull the data
 source unpack_data.sh
+
+python3 check_symbolic_properties.py -f ./study_data/scenarios/ -s ./results/
+python3 check_symbolic_properties.py -f ./study_data/tcp/run1/ -s ./results/
+python3 check_symbolic_properties.py -f ./study_data/lav/run1/ -s ./results/
+python3 check_symbolic_properties.py -f ./study_data/interfuser/run1/ -s ./results/
--- unpack_data.sh
+++ unpack_data.sh
@@ -1,6 +1,6 @@
-if test -f "./study_data/" ; then
+if test -d "./study_data/" ; then
   printf "Data has already been unpacked to ./study_data/, skipping.\n"
 else
   printf "Unpacking study_data.7z to ./study_data/. Will result in ~1.1GB.\n"
-  7z x study_data.7z -o./study_data/
+  7z x study_data.7z -o./
 fi
